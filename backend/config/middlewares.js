3c6ec314db182557946a48c5f6c83c08c608cda1
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2019-04-06 20:55:14 -0400

    Heroku config

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const cors = require('cors')
+
 const bodyParser = require('body-parser')
 
 
 module.exports = app => {
-    app.use(cors())
+
     app.use(bodyParser.json())
     // app.use(bodyParser.urlencoded({extended: false}))
     app.use('/temp', express.static('temp'))
